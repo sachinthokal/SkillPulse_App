85cfb03872266fb9fe0416fb3516588be6bc9d55
sachinthokal <sachinthokal1799@gmail.com>
2026-05-24 08:19:29 +0000

    feat : non-prod infra env added

---
+++ Terraform/outputs.tf
@@ -0,0 +1,16 @@
+output "kubeconfig" {
+  value     = kind_cluster.default.kubeconfig
+  sensitive = true
+}
+
+output "cluster_name" {
+  value = kind_cluster.default.name
+}
+
+output "cluster_ports" {
+  value = var.cluster_ports
+}
+
+output "cluster_node_version" {
+  value = var.node_image
+}
